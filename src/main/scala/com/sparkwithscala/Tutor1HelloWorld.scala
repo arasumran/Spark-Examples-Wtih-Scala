12814a4c616efca8ef9f0cea2ed52ebec2f694e6
oredata <oredata@oredatas-MacBook-Pro.local>
2021-06-30 16:41:50 +0300

    Initial commit

---
+++ src/main/scala/com/sparkwithscala/Tutor1HelloWorld.scala
@@ -0,0 +1,21 @@
+package com.sparkwithscala
+
+
+import org.apache.spark._
+import org.apache.log4j._
+
+object Tutor1HelloWorld {
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sc = new SparkContext("local[*]", "HelloWorld")
+
+    val lines = sc.textFile("data/ml-100k/u.data")
+    val numLines = lines.count()
+
+    println("Hello world! The u.data file has " + numLines + " lines.")
+
+    sc.stop()
+  }
+}
